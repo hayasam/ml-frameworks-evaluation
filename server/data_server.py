b9c0ee7c8a385d138e973ffec4a879fde579d319
EmilioRivera <emilio.rivera@polymtl.ca>
2019-08-11 20:40:20 -0400

    Default seed control file options are set via options specified in command line

--- server/data_server.py
+++ server/data_server.py
@@ -184,7 +184,8 @@ def parse_args():
 
 if __name__ == "__main__":
     ARGS = parse_args()
-    SEED_CONTROLLER = SeedController.from_saved_file(ARGS['seed_controller_file'])
+    non_existing_kwargs = {'seed_len': ARGS['default_minimal_seed_len'], 'min_val': ARGS['default_min_seed_value'], 'max_val': ARGS['default_max_seed_value']]}
+    SEED_CONTROLLER = SeedController.from_saved_file(ARGS['seed_controller_file'], **non_existing_kwargs)
     LOGGER_STORE = MetricsLoggerStore(base_path=ARGS['metrics_log_dir'])
     SERVER_LOGGER = logging.getLogger('server')
     SERVER_LOGGER.addHandler(logging.FileHandler(ARGS['server_log_file']))
