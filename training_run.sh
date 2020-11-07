0b260733c8ac5face38261262f95422ee46e3a75
EmilioRivera <emilio.rivera@polymtl.ca>
2020-11-07 05:14:34 -0500

    Adapt existing script to launch multiple runs automatically

--- training_run.sh
+++ training_run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-
+# Expected to be called with X_DO_LAUNCH=1
 MODELS=( "EvaluationVGG" )
-BASE_DIR=$(readlink -f envs/pytorch/)
+BASE_DIR=${1:?First argument needs to be path to folder containing experiment configurations}
 EXPERIMENTS_DIR=( $(ls "$BASE_DIR") )
 CHALLENGE="cifar"
 NUM_CLASSES=10
@@ -12,14 +12,15 @@ for n in "${EXPERIMENTS_DIR[@]}"; do
     echo "$exp_name"
     for model in "${MODELS[@]}"; do
         for evaluationtype in "buggy" "corrected"; do
-            echo "RUNNING EXP ${exp_name} - ${model} for evaluation type $evaluationtype."
-            envfile_path="${BASE_DIR}/${exp_name}/${evaluationtype}.env"
-            echo "Content of envfile: "
-            echo "------"
-            cat "$envfile_path"
-            echo "======"
+            # echo "RUNNING EXP ${exp_name} - ${model} for evaluation type $evaluationtype."
+            # envfile_path="${BASE_DIR}/${exp_name}/${evaluationtype}.env"
+            # echo "Content of envfile: "
+            # echo "------"
+            # cat "$envfile_path"
+            # echo "======"
 
-            echo docker run --name "${exp_name}_${evaluationtype}_${model}" --mount source=pip-cache,target=/pip-cache --mount source=results,target=/results --mount source=build-vol,target=/builds --gpus all --env-file "$envfile_path" --env MODEL_NAME="$model" --env CHALLENGE="$CHALLENGE" --env NUM_CLASSES="$NUM_CLASSES" -it emiliorivera/ml-frameworks:eval100_client
+            X_DO_LAUNCH=${X_DO_LAUNCH:-0} ./run_launcher.sh ${exp_name} ${evaluationtype} $model
+            # echo docker run --name "${exp_name}_${evaluationtype}_${model}" --mount source=pip-cache,target=/pip-cache --mount source=results,target=/results --mount source=build-vol,target=/builds --gpus all --env-file "$envfile_path" --env MODEL_NAME="$model" --env CHALLENGE="$CHALLENGE" --env NUM_CLASSES="$NUM_CLASSES" -it emiliorivera/ml-frameworks:eval100_client
         done
     done
 done
