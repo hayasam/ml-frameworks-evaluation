5846241c58f73efb6de476cd04bc5ad8b6c07d7f
EmilioRivera <emilio.rivera@polymtl.ca>
2020-11-07 00:18:21 -0500

    Change server launch script to have options for seed without defaults

--- src/server.sh
+++ src/server.sh
@@ -11,8 +11,8 @@ conda activate ${CONDA_SERVER_ENV:-server}
 PY_CACHE_DIR=${PY_CACHE_DIR:-/pip-cache}
 DATA_SERVER_DATA_ROOT="${DATA_SERVER_DATA_ROOT:?}"
 METRICS_LOG_DIR="${METRICS_LOG_DIR:-/results/server}"
-SEED_CONTROLLER_FILE="${SEED_CONTROLLER_FILE:-/results/server/seed_control}"
-SERVER_LOG_FILE="${METRICS_LOG_DIR}/${SERVER_LOG_FILE:-server.log}"
+SEED_CONTROLLER_FILE="${SEED_CONTROLLER_FILE:?}"
+SERVER_LOG_FILE="${SERVER_LOG_FILE:-${METRICS_LOG_DIR/server.log}}"
 
 # Creating server directory
 mkdir -p "$METRICS_LOG_DIR"
@@ -24,4 +24,4 @@ fi
 
 cd server
 echo "Starting server..."
-python data_server.py --metrics-log-dir "$METRICS_LOG_DIR" --server-log-file "$SERVER_LOG_FILE" --seed-controller-file "$SEED_CONTROLLER_FILE" --data-root "$DATA_SERVER_DATA_ROOT"
+python data_server.py --metrics-log-dir "$METRICS_LOG_DIR" --server-log-file "$SERVER_LOG_FILE" --seed-controller-file "$SEED_CONTROLLER_FILE"  ${DEFAULT_MINIMAL_SEED_LEN:+ --default-minimal-seed-len $DEFAULT_MINIMAL_SEED_LEN} ${DEFAULT_MIN_SEED_VALUE:+ --default-min-seed-value $DEFAULT_MIN_SEED_VALUE} ${DEFAULT_MAX_SEED_VALUE:+ --default-max-seed-value $DEFAULT_MAX_SEED_VALUE}
