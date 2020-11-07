a30e5dee78682c1bebc7b571e2a55e0551f8693c
EmilioRivera <emilio.rivera@polymtl.ca>
2020-11-06 21:55:15 -0500

    Change the base image for client to dissociate from builder

--- build_images.sh
+++ build_images.sh
@@ -2,9 +2,9 @@
 
 # Build the clients
 ## Version Python 3.7.9
-sudo docker build -f Dockerfile.client --build-arg BASE_IMAGE_VERSION=py379-cu101 --tag emiliorivera/ml-frameworks-evaluation-client:py379-cu101 .
+sudo docker build -f Dockerfile.client --build-arg BASE_IMAGE_VERSION="10.1-cudnn7-runtime-ubuntu18.04" --build-arg CLIENT_PYTHON_VERSION="3.7.9" --tag emiliorivera/ml-frameworks-evaluation-client:py379-cu101 .
 ## Version Python 3.6.7
-sudo docker build -f Dockerfile.client --build-arg BASE_IMAGE_VERSION=py367-cu101 --tag emiliorivera/ml-frameworks-evaluation-client:py367-cu101 .
+sudo docker build -f Dockerfile.client --build-arg BASE_IMAGE_VERSION="10.1-cudnn7-runtime-ubuntu18.04" --build-arg CLIENT_PYTHON_VERSION="3.6.7" --tag emiliorivera/ml-frameworks-evaluation-client:py367-cu101 .
 
 # Build the server
 sudo docker build -f Dockerfile.server --tag emiliorivera/ml-frameworks-evaluation-server .
