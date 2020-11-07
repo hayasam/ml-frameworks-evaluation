ae6a37cfefbb70ab79123254870b19880adbe16c
EmilioRivera <emilio.rivera@polymtl.ca>
2020-11-06 19:10:35 -0500

    Change client Docker image names
    For a clearer separation of client and server

--- build_images.sh
+++ build_images.sh
@@ -2,8 +2,8 @@
 
 # Build the clients
 ## Version Python 3.7.9
-sudo docker build -f Dockerfile.client --build-arg BASE_IMAGE_VERSION=py379-cu101 --tag emiliorivera/ml-frameworks-evaluation:py379-cu101 .
+sudo docker build -f Dockerfile.client --build-arg BASE_IMAGE_VERSION=py379-cu101 --tag emiliorivera/ml-frameworks-evaluation-client:py379-cu101 .
 ## Version Python 3.6.7
-sudo docker build -f Dockerfile.client --build-arg BASE_IMAGE_VERSION=py367-cu101 --tag emiliorivera/ml-frameworks-evaluation:py367-cu101 .
+sudo docker build -f Dockerfile.client --build-arg BASE_IMAGE_VERSION=py367-cu101 --tag emiliorivera/ml-frameworks-evaluation-client:py367-cu101 .
 
 # Build the server
