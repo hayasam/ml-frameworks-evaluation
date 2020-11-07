f3c1a46b209df62681074bdce0233a2cbc129d07
EmilioRivera <emilio.rivera@polymtl.ca>
2020-11-06 23:37:51 -0500

    Add ability to client to install specific requirements file

--- src/client.sh
+++ src/client.sh
@@ -1,24 +1,29 @@
 #!/usr/bin/env bash
 
 source /etc/profile.d/conda.sh
-conda activate ${CONDA_TRAIN_ENV:-train}
 
 # TODO: Check how these variables should be set
+# Information about the experiment
 BUG_NAME="${BUG_NAME:?}"
 EVALUATION_TYPE="${EVALUATION_TYPE:?}"
-CLIENT_VENV_NAME="${BUG_NAME}_${EVALUATION_TYPE}"
 CLIENT_MANUAL_DEPENDENCY="${CLIENT_MANUAL_DEPENDENCY:?}"
 CLIENT_PY_VERSION="${CLIENT_PY_VERSION:?}"
+NUMBER_OF_RUNS="${NUMBER_OF_RUNS:?}"
+# Challenge information
 NUM_CLASSES="${NUM_CLASSES:?}"
 CHALLENGE="${CHALLENGE:?}"
 MODEL_LIBRARY="${MODEL_LIBRARY:?}"
 MODEL_NAME="${MODEL_NAME:?}"
-CLIENT_LOG_DIR="${CLIENT_LOG_DIR:?}"
-NUMBER_OF_RUNS="${NUMBER_OF_RUNS:?}"
-PY_CACHE_DIR=${PY_CACHE_DIR:-/pip-cache}
-DATA_SERVER_ENDPOINT="${DATA_SERVER_ENDPOINT:?}"
+# Oher
 USE_BUILD_MKL="${USE_BUILD_MKL:-0}"
+DATA_SERVER_ENDPOINT="${DATA_SERVER_ENDPOINT:?}"
+# Computed attribute
+CLIENT_VENV_NAME="${BUG_NAME}_${EVALUATION_TYPE}"
+# Mounts and directories
+CLIENT_LOG_DIR="${CLIENT_LOG_DIR:?}"
+PY_CACHE_DIR=${PY_CACHE_DIR:-/pip-cache}
 BUILD_DIR="${BUILD_DIR:?}"
+CONFIG_DIR="${CONFIG_DIR:?}"
 
 # Setting PIP_FIND_LINKS will allow to check the local
 # directory
@@ -36,10 +41,9 @@ mkdir -p "$CLIENT_LOG_DIR"
 # Client configuration
 #
 cd client
-# mkdir --parents .logs
-pyenv local "$CLIENT_PY_VERSION"
-python -m venv ".venvs/$CLIENT_VENV_NAME"
-source ".venvs/$CLIENT_VENV_NAME/bin/activate"
+# Replicate the first train environment to branch off
+conda create --name ${CLIENT_VENV_NAME} --clone ${CONDA_TRAIN_ENV:-train}
+conda activate ${CLIENT_VENV_NAME}
 
 # Create temporary file that pip will read in order to allow caching
 echo "[global]" >> .pip.config
@@ -52,8 +56,45 @@ if [ "${#MANUAL_WHL[@]}" -ne 1 ]; then
   exit 1
 fi
 
-# Intall everything in one go
-pip install ./shared ${MANUAL_WHL[0]} -r ./client/requirements.in
+CONFIG_BUG_DIR="${CONFIG_DIR}/${BUG_NAME}"
+
+# Make sure bug specific directory exists
+# It is used to share runtime dependencies
+if [ ! -d "${CONFIG_BUG_DIR}" ]; then
+  1>2& echo "Directory ${CONFIG_BUG_DIR} does not exist! Exiting..."
+  exit 1
+fi
+
+if [ -e "${CONFIG_BUG_DIR}/requirements.txt" ]; then
+  # Try installing it
+  pip install -r "${CONFIG_BUG_DIR}/requirements.txt"
+else
+  # Create a bug specific requirements.txt
+  # Bug specific requirements.in
+  if [ -e "${CONFIG_BUG_DIR}/requirements.in" ]; then
+    pip install -r "${CONFIG_BUG_DIR}/requirements.in"
+    # Save output so next runner can take it
+    pip freeze -l > "${CONFIG_BUG_DIR}/requirements.txt"
+  elif [ -e ./client/requirements.in ]; then
+    # fall back on client requirements.in
+    # which does not specify versions
+    pip install -r ./client/requirements.in
+    # Save output so next runner can take it
+    pip freeze -l > "${CONFIG_BUG_DIR}/requirements.txt"
+  else
+    1>2& echo "No suitable requirements file found was found! Exiting..."
+    exit 1
+  fi
+  # Record what environment created the requirements.txt
+  echo "${EVALUATION_TYPE}" > "${CONFIG_BUG_DIR}/setup_by.txt"
+fi
+
+# Add additional dependencies
+pip install ./shared ${MANUAL_WHL[0]}
+
+# Everything installed, export the environment information
+conda list --export > "${CONFIG_BUG_DIR}/${EVALUATION_TYPE}.export.txt"
+conda list --explicit > "${CONFIG_BUG_DIR}/${EVALUATION_TYPE}.explicit.txt"
 
 echo "Starting client..."
 # Note: runs should maybe be parameterized?
