616915ead70b814eb29854759fab8f15a676ade0
EmilioRivera <emilio.rivera@polymtl.ca>
2020-11-06 19:09:46 -0500

    Change how evaluation server Docker gets built and launched

--- src/server.sh
+++ src/server.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+#
+# Server configuration
+#
+source /etc/profile.d/conda.sh
+conda activate ${CONDA_SERVER_ENV:-server}
+
 # TODO: Maybe check if dependencies are already satisfied
 # Set PY_CACHE_DIR
 PY_CACHE_DIR=${PY_CACHE_DIR:-/pip-cache}
-SERVER_PY_VERSION="${SERVER_PY_VERSION:-3.6.8}"
-SERVER_VENV_NAME="server-venv"
 DATA_SERVER_DATA_ROOT="${DATA_SERVER_DATA_ROOT:?}"
 METRICS_LOG_DIR="${METRICS_LOG_DIR:-/results/server}"
 SEED_CONTROLLER_FILE="${SEED_CONTROLLER_FILE:-/results/server/seed_control}"
@@ -18,28 +22,6 @@ if [ ! -d "$(dirname $SEED_CONTROLLER_FILE)" ]; then
   exit 1
 fi
 
-#
-# Server configuration
-#
 cd server
-# mkdir --parents .logs
-pyenv local $SERVER_PY_VERSION
-python -m venv ".venvs/$SERVER_VENV_NAME"
-source ".venvs/$SERVER_VENV_NAME/bin/activate"
-
-# Create temporary file that pip will read in order to allow caching
-echo "[global]" >> .pip.config
-echo "cache-dir = $PY_CACHE_DIR" >> .pip.config
-export PIP_CONFIG_FILE=".pip.config"
-
-if [[ ! $(python --version) = "Python $SERVER_PY_VERSION" ]]; then
-  echo "$(python --version)"
-  echo 'Python server version not correctly set.'
-  echo 'Exiting...'
-  exit 1
-fi
-
-pip install ../shared
-pip install -r requirements.txt
 echo "Starting server..."
 python data_server.py --metrics-log-dir "$METRICS_LOG_DIR" --server-log-file "$SERVER_LOG_FILE" --seed-controller-file "$SEED_CONTROLLER_FILE" --data-root "$DATA_SERVER_DATA_ROOT"
